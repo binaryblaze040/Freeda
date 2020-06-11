2e3f81a63400281a76f9eed676fcdb50cbfca64d
1706040 <1706040@kiit.ac.in>
2020-06-11 16:17:52 +0530

    allFiles

--- src/views/CollectorDashboard.js
+++ src/views/CollectorDashboard.js
@@ -114,7 +114,6 @@ class CollectorDashboard extends Component {
         <Grid style={{backgroundColor: '#252C48', boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
         <EnhancedTable />
         </Grid>
-
         {this.state.redirect === true && (
           <Redirect
             to={{
